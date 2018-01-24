fcc5b9e3f59be272483e4ab0deaf45c7768f0e23
jayakrishna <jayakrishna.macharla@gmail.com>
2018-01-24 20:19:28 +0530

    added the new files

---
+++ HelloWorld/Assets/Scripts/NotificationManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Initialize ();	
+	}
+
+	void Initialize()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
